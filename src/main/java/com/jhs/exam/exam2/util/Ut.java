a61a906d915c5eb8fddb479708e5c03c85d3de57
jhs512 <jangka512@gmail.com>
2021-07-19 12:42:03 +0900

    현재 URI 얻는 기능, 로그인 후 원래 페이지로 이동 기능

--- src/main/java/com/jhs/exam/exam2/util/Ut.java
+++ src/main/java/com/jhs/exam/exam2/util/Ut.java
@@ -1,5 +1,6 @@
 package com.jhs.exam.exam2.util;
 
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -102,4 +103,12 @@ public class Ut {
 			return null;
 		}
 	}
+	
+	public static String getUriEncoded(String str) {
+        try {
+            return URLEncoder.encode(str, "UTF-8");
+        } catch (Exception e) {
+            return str;
+        }
+    }
 }
